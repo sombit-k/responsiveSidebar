fb4a269d451745a27b447b579b0550a2ae5da087
sombit-k <sombitkarmakar018@gmail.com>
2024-07-04 18:22:49 +0530

    adding code

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ let barmenu=document.querySelector("#navbar .icon i");
 console.dir(barmenu);
 
 barmenu.addEventListener("click",(event)=>{
-    // console.log("event",event);
-    // barmenu.style="display:none";
-    barmenu.setAttribute("class","disappear")
-})
+    barmenu.setAttribute("id","disappear");
+    console.log(barmenu)
+    setTimeout(()=>{
+    barmenu.style="display:none"
+    },750)
+});
+
+
