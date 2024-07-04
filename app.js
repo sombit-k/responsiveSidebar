050a4ea0bd82881e0a60b758c8bc1f3eee8e18fe
sombit-k <sombitkarmakar018@gmail.com>
2024-07-04 19:26:15 +0530

    added corrections

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ barmenu.addEventListener("click",(event)=>{
     barmenu.setAttribute("id","disappear");
     console.log(barmenu)
     setTimeout(()=>{
-    barmenu.style="display:none"
+    barmenu.setAttribute("id","dissapeared");
     },750);
     showSidebar();
 });
+let showBarMenu =() =>{
+    let barmenu=document.querySelector("#navbar .icon i");
+    barmenu.setAttribute("id","");
+    barmenu.setAttribute("style","");
+    console.log("showing barmenu")
+}
 
 let showSidebar =() =>{
     let sidebar = document.querySelector(".sidebar");
@@ -16,7 +22,16 @@ let showSidebar =() =>{
     sidebar.setAttribute("id","sidebar");
     let cross = document.querySelector(".crossIcon");
     cross.addEventListener("click",()=>{
-        console.log("cross pressed")
+        hideSidebar(sidebar);
     })
+}
+
+let hideSidebar = (sidebar) => {
+    console.log("Hiding sidebar");
+    sidebar.setAttribute("id","hideSidebar");
+    setTimeout(()=>{
+    sidebar.setAttribute("id","hiddenSidebar");
+    },2000)
+    showBarMenu();
 }
 
