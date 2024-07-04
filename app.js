a7e17b5456421cc987b05472c54248c41e0b1a26
sombit-k <sombitkarmakar018@gmail.com>
2024-07-04 18:55:58 +0530

    added lines of codes

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+
 let barmenu=document.querySelector("#navbar .icon i");
 console.dir(barmenu);
-
 barmenu.addEventListener("click",(event)=>{
     barmenu.setAttribute("id","disappear");
     console.log(barmenu)
     setTimeout(()=>{
     barmenu.style="display:none"
-    },750)
+    },750);
+    showSidebar();
 });
 
+let showSidebar =() =>{
+    let sidebar = document.querySelector(".sidebar");
+    console.log(sidebar);
+    sidebar.setAttribute("id","sidebar");
+}
 
